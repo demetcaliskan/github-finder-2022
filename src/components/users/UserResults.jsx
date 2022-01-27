bf1af0a4dd05386a1d96ef34f338fd9fd3d818b5
demetcaliskan <demetcaliskann@gmail.com>
2022-01-28 01:46:55 +0300

    get users with reducers and not fetch users

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,28 +1,12 @@
-import { useEffect, useState } from 'react'
+import { useContext } from 'react'
 import Spinner from '../layout/Spinner'
 import UserItem from './UserItem'
+import GithubContext from '../../context/github/GithubContext'
 
 function UserResults() {
-  const [users, setUsers] = useState([])
-  const [loading, setLoading] = useState(true)
 
-  useEffect(() => {
-    fetchUsers()
-  }, [])
+  const { users, loading } = useContext(GithubContext)
 
-  const fetchUsers = async () => {
-    const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
-      headers: {
-        Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
-      }
-    })
-    const data = await response.json()
-
-    console.log(data)
-
-    setUsers(data)
-    setLoading(false)
-  }
 
   if (!loading) {
     return (
